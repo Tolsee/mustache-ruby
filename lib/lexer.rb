488e7775a3d09cb55c56bee4f8a79bc5e57813de
Tolsee <tolsee3@gmail.com>
2020-01-05 13:48:12 +0700

    Improve expression match - Match any spaces inside the braces - Match valid ruby variable

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -1,21 +1,25 @@
 require 'strscan'
-require 'pry'
 
-# Lexer is responsible for creating tokens from the template string
+#
+# Lexer
+# Responsible for creating tokens from the template string
+# [[:TOKEN, value]]
 #
 class Lexer
-  attr_accessor :stack, :tokens
-  attr_reader :scanner
-
   def initialize
     @stack = []
     @tokens = []
   end
 
+  # @param [string] template
+  # @return [Array] tokens
   def self.tokenize(template)
     new.tokenize(template)
   end
 
+
+  # @param [string] template
+  # @return [Array] tokens
   def tokenize(template)
     @scanner = StringScanner.new(template)
 
@@ -32,6 +36,9 @@ class Lexer
 
   private
 
+  attr_accessor :stack, :tokens
+  attr_reader :scanner
+
   def default_state
     if scanner.scan(/{{/)
       open_expression
@@ -45,10 +52,18 @@ class Lexer
     if scanner.scan(/}}/)
       close_expression
       pop_state
-    elsif scanner.scan(/[\w]+/)
-      content(scanner.matched)
+    elsif scanner.scan(identifier_regex)
+      identifier(scanner.matched)
     end
+  end
 
+  def identifier_regex
+    %r{
+      \s*           # Can have any number of space after braces
+      [a-z_]        # Starting of variable can contain a-z or underscore
+      [a-zA-Z_0-9]* # Variable may contain a-z, A-Z, 0-9 or underscore, can have any number of occurrence
+      \s*           # Can have any number of space before braces
+    }x
   end
 
   def push_state(current_state)
@@ -73,5 +88,9 @@ class Lexer
 
   def content(content)
     tokens << [:CONTENT, content]
+  end
+
+  def identifier(identifier)
+    tokens << [:IDENTIFIER, identifier.strip]
   end
 end
--- spec/lib/lexer_spec.rb
+++ spec/lib/lexer_spec.rb
@@ -1,7 +1,23 @@
 describe Lexer do
   let(:lexer) { described_class }
 
-  it 'detects content expression' do
+  it 'detects content expression with spaces' do
+    template = 'hello, {{ name }}'
+
+    tokens = lexer.tokenize(template)
+
+    expect(tokens[0][0]).to eq(:CONTENT)
+    expect(tokens[0][1]).to eq('hello, ')
+
+    expect(tokens[1][0]).to eq(:OPEN_EXPRESSION)
+
+    expect(tokens[2][0]).to eq(:IDENTIFIER)
+    expect(tokens[2][1]).to eq('name')
+
+    expect(tokens[3][0]).to eq(:CLOSE_EXPRESSION)
+  end
+
+  it 'detects content expression with out spaces' do
     template = 'hello, {{name}}'
 
     tokens = lexer.tokenize(template)
@@ -11,9 +27,35 @@ describe Lexer do
 
     expect(tokens[1][0]).to eq(:OPEN_EXPRESSION)
 
-    expect(tokens[2][0]).to eq(:CONTENT)
+    expect(tokens[2][0]).to eq(:IDENTIFIER)
     expect(tokens[2][1]).to eq('name')
 
     expect(tokens[3][0]).to eq(:CLOSE_EXPRESSION)
+  end
+
+  it 'detects content expression with valid ruby variable' do
+    template = 'hello, {{ _name1_X }} {{ age_8 }}'
+
+    tokens = lexer.tokenize(template)
+
+    expect(tokens[0][0]).to eq(:CONTENT)
+    expect(tokens[0][1]).to eq('hello, ')
+
+    expect(tokens[1][0]).to eq(:OPEN_EXPRESSION)
+
+    expect(tokens[2][0]).to eq(:IDENTIFIER)
+    expect(tokens[2][1]).to eq('_name1_X')
+
+    expect(tokens[3][0]).to eq(:CLOSE_EXPRESSION)
+
+    expect(tokens[4][0]).to eq(:CONTENT)
+    expect(tokens[4][1]).to eq(' ')
+
+    expect(tokens[5][0]).to eq(:OPEN_EXPRESSION)
+
+    expect(tokens[6][0]).to eq(:IDENTIFIER)
+    expect(tokens[6][1]).to eq('age_8')
+
+    expect(tokens[7][0]).to eq(:CLOSE_EXPRESSION)
   end
 end
